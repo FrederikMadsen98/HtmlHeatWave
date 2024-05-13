d168bb002d430fd5b8475946302f73d9ee7cfc39
Frederik Madsen <fmadsen1.10.98@gmail.com>
2024-05-13 13:57:41 +0200

    Forsøg på at tilføje local weather til html og js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const baseUrl = "https://heatwaveprojekt.azurewebsites.net/api/temp";
+const ApiUrl = "http://api.weatherapi.com/v1/current.json";
+const ApiKey = "29501236f4854c099e1104709240405";
 
 Vue.createApp({
     data() {
@@ -15,6 +17,22 @@ Vue.createApp({
             try {
                 const response = await axios.get(baseUrl);
                 this.TempMeasurements = response.data;
+            } catch (error) {
+                this.error = error.message;
+            }
+        },
+        async getLocalTemperature() {
+            try {
+                const response = await axios.get(openWeatherMapApiUrl);
+                this.localTemperature = response.data.main.temp;
+            } catch (error) {
+                this.error = error.message;
+            }
+        },
+        async getLocalTemperature() {
+            try {
+                const response = await axios.get(ApiUrl);
+                this.localTemperature = response.data.main.temp;
             } catch (error) {
                 this.error = error.message;
             }
