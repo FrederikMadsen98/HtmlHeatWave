83273620f85934b4b007ed5d657bddbd3bc92d3a
Frederik Madsen <fmadsen1.10.98@gmail.com>
2024-05-22 11:30:11 +0200

    Forsøg på values i slider

--- index.js
+++ index.js
@@ -52,16 +52,16 @@ Vue.createApp({
         },
         toggleTable() {
             this.showTable = !this.showTable;
-        }
-    },
-}
-).mount('#app');
+        },
+        mounted() {
+            var slider = document.getElementById("myRange");
+            var output = document.getElementById("demo");
+            output.innerHTML = slider.value; // Display the default slider value
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function () {
-    output.innerHTML = this.value;
-}
+            // Update the current slider value (each time you drag the slider handle)
+            slider.oninput = function () {
+                output.innerHTML = this.value;
+            }
+        },
+    }
+}).mount('#app');
